4f5999ae12c66902fe550f78801c79e6c1453398
Mirkoto6 <Mirko_2005@abv.bg>
2023-10-21 13:11:11 +0300

    The End

---
+++ ApartmentRent1.Services/Program.cs
@@ -0,0 +1,25 @@
+﻿using ApartmentRent.Data;
+using ApartmentRent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Services
+{
+    public class CarsService
+    {
+        AppDbContext db = new AppDbContext();
+
+        public List<ApartmentRent.Data.Apartment> GetCarsByBrand(string location)
+        {
+            return db.Apartments
+                .Where(x => x.Location == location)
+                .Select(c => new ApartmentRent.Data.Apartment() { Location = c.Location, Bedrooms = c.Bedrooms })
+                .ToList();
+        }
+    }
+
+
+}
